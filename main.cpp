dedee4f58d6b96814b9121900db4f0e8ac24427b
Pablo C P <pcobper@upv.edu.es>
2024-11-29 12:34:28 +0100

    implementado el test del brazo robot en main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "BrazoRobotico.h"
+#include <iostream>
+
+int main(){
+
+	BrazoRobotico brazo(0,0,0);
+	
+	std::cout << "Posición actual: (" << brazo.get_x() << ", " << brazo.get_y() << ", " << brazo.get_z() << ")\n";
+
+	brazo.mover(10, 20, 30);
+
+	std::cout << "Posición actual: (" << brazo.get_x() << ", " << brazo.get_y() << ", " << brazo.get_z() << ")\n";
+
+	brazo.coger();
+
+	std::cout << "Sujetando objeto = " << brazo.sujetando_objeto() << "\n";
+
+	brazo.soltar();
+
+	std::cout << "Sujetando objeto = " << brazo.sujetando_objeto() << "\n";
+
+	return 0;
+}
